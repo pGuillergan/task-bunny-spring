cde370d5bb1112ba7fab59f341e8f38cef65c4d5
Tharindra Paranagama <tharindrapara@gmail.com>
2020-06-15 13:31:09 -0700

    dealing with CORS

--- src/main/java/com/taskbunny/config/SecurityConfiguration.java
+++ src/main/java/com/taskbunny/config/SecurityConfiguration.java
@@ -33,6 +33,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
 		http.authorizeRequests()
 			.antMatchers("/users").hasRole("CLIENT")
 //			.antMatchers("/users").hasAnyRole("client", "provider")
